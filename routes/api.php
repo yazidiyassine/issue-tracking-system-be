7d3c4bd774b89d9c4f413cfab4824f5ccb7c04c4
yazidiyassine <yassine.yzd@gmail.com>
2022-07-12 16:26:52 +0100

    config cors

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/user', function (Request $request) {
-    echo 'tst api';
+    return response() -> json(['success' => true]);
 });
